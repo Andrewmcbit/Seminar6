d86492fc3367989a601a559afccf1fb2966ff6e4
Andrew <1.smirnov.a.v.1@gmail.com>
2023-12-05 20:44:22 +0700

    третье задание

---
+++ 3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.*/
+
+void IsPalindrome(string inputString){
+    bool result = true;
+    int i = 0;
+    while ((result != false) && (i < inputString.Length/2)){
+        if (inputString[i] == inputString[inputString.Length - 1 - i]){
+            result = true;
+        } else {
+            result = false;
+        }
+        i++;
+    }
+    Console.Write(inputString);
+    if (result == true) {
+        Console.Write(" => Да");
+    } else {
+        Console.Write(" => Нет");
+    }
+    Console.WriteLine();
+}
+
+string str = "“aBcD1ef!-";
+IsPalindrome(str);
+str = "шалаш";
+IsPalindrome(str);
+str = "5445";
+IsPalindrome(str);
