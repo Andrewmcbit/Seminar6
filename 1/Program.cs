b6203eceb4e7fb6b9d8d63b42a36a006a0cd5f31
Andrew <1.smirnov.a.v.1@gmail.com>
2023-12-05 20:44:05 +0700

    первое задание

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.*/ 
+
+string GetStringFromChar(char[,] inputArray){
+    string outputString = "";
+    for (int i = 0; i < inputArray.GetLength(0); i++){
+        for (int j = 0; j < inputArray.GetLength(1); j++){
+            outputString += inputArray[i,j];
+        }
+    }
+    return outputString;
+}
+
+char[,] array = {{'a','b','c','d'},{'e','f','g','h'}};
+string str = GetStringFromChar(array);
+Console.Write(str);
