64c4c74a94074d3b9dd1be6c3f6407400a593b14
Andrew <1.smirnov.a.v.1@gmail.com>
2023-12-05 20:44:13 +0700

    второе задание

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+Сформируйте строку, в которой все заглавные буквы заменены на строчные.*/ 
+
+void GetLowString (string inputString){
+    Console.WriteLine(inputString.ToLower());
+    foreach (char c in inputString){
+        Console.Write(Char.ToLower(c) + " ");
+    }
+}
+
+string str = "aBcdeFgH1";
+Console.WriteLine(str);
+GetLowString(str);
